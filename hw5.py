cd8f55446de7aa984949c836f869f6bf1994df8e
EsterG <eddie_g_89@hotmail.com>
2014-04-02 17:44:55 -0700

    Finished naive decision tree class

--- hw5.py
+++ hw5.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import pylab as plt
 import scipy.io as sio
@@ -11,16 +12,21 @@ X = data['Xtrain']
 y = data['ytrain']
 Xtest = data['Xtest']
 
-# print train.shape
-# print target.shape
-# print test.shape
-
 # binarize features for easy decision tree use
 X = (X > 0) # convert train/test into matrix of logicals
 Xtest = (Xtest > 0)
+y = (y > 0)
 
-# calculate H(X)
+#calculate H(X)
 #data_entropy = H(y) # ~0.97
 #print data_entropy
 
-#dt = DecisionTree(X,y)
+
+print "Initilizaing/Training decision tree"
+dt = DecisionTree(X,y)
+print "Finished training"
+
+print "Classifying training set"
+pred = dt.classify(X)
+
+print "Training set error:"+str((pred!=y).sum()/y.size)
